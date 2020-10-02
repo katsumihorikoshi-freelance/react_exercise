34290b513df8079d3ecea8b77d48cb8881643092
KatsumiHorikoshi <horikoshi@freebal.com>
2020-10-02 17:42:36 +0900

    succeed routing frontend to backend(cloud functions)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+
 import './index.css';
+import axios from 'axios';
 import App from './App';
+import firebaseConfig from './firebase-config';
+
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp(firebaseConfig);
+
+void axios.get('/members').then((res) => console.dir(res));
+
+console.log('hoge');
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
